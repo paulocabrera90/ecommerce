77fa3480ca116603e00959631418d8e75d1a1209
Paulo Cabrera <paulocabrera90@gmail.com>
2022-03-16 20:56:56 -0300

    Enviar al carrito.html cantidad y id

--- src/main/java/com/infosl/ecommerce/controller/HomeController.java
+++ src/main/java/com/infosl/ecommerce/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.infosl.ecommerce.controller;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import org.slf4j.Logger;
@@ -9,8 +11,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.infosl.ecommerce.model.DetalleOrden;
+import com.infosl.ecommerce.model.Orden;
 import com.infosl.ecommerce.model.Producto;
 import com.infosl.ecommerce.service.producto.ProductoService;
 
@@ -22,6 +28,11 @@ private final Logger LOGGER = LoggerFactory.getLogger(HomeController.class);
 	
 	@Autowired
 	private ProductoService productoService;
+	
+	//Almacenar los detalles de orden
+	List<DetalleOrden> detalleOrdens = new ArrayList<DetalleOrden>();
+	//Datos de la orden
+	Orden orden = new Orden();
 	
 	@GetMapping("")
 	public String home(Model model) {
@@ -42,5 +53,18 @@ private final Logger LOGGER = LoggerFactory.getLogger(HomeController.class);
 		model.addAttribute("prodObtenido", producto);		
 	
 		return "usuario/productohome";
+	}
+	
+	@PostMapping("/carrito")
+	public String addCarrito(@RequestParam Integer pro_id, @RequestParam Integer cantidad) {
+		DetalleOrden detalleOrden = new DetalleOrden();
+		Producto producto= new Producto();
+		double sumaTotal= 0;
+		
+		Optional<Producto> optionalProducto = productoService.get(pro_id);
+		producto = optionalProducto.get();
+		LOGGER.info("Objeto producto al carrito {}", producto);
+		LOGGER.info("Cantidad: {}", cantidad);
+		return "usuario/carrito";
 	}
 }
