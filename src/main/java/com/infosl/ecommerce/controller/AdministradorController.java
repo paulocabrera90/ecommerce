0f3f523f70ae9271ec618591a9059a8cdc712150
Paulo Cabrera <paulocabrera90@gmail.com>
2022-03-20 01:20:00 -0300

    funcionalidad lista de usuarios

--- src/main/java/com/infosl/ecommerce/controller/AdministradorController.java
+++ src/main/java/com/infosl/ecommerce/controller/AdministradorController.java
@@ -7,16 +7,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.infosl.ecommerce.model.Producto;
+import com.infosl.ecommerce.model.Usuario;
 import com.infosl.ecommerce.service.producto.IProductoService;
+import com.infosl.ecommerce.service.usuario.IUsuarioService;
  
 @Controller
 @RequestMapping("/administrador")
 public class AdministradorController {
+	private final Logger LOGGER = LoggerFactory.getLogger(AdministradorController.class);
 	
 	@Autowired
 	private IProductoService productoService;
+	@Autowired
+	private IUsuarioService usuarioService;
 	
 	@GetMapping("")
 	public String home(Model model) {
@@ -25,5 +32,15 @@ public class AdministradorController {
 		model.addAttribute("listaProductos" ,listProductos);
 		
 		return "administrador/home";
+	}
+	
+	@GetMapping("/usuarios")
+	public String findUsuarios(Model model) {
+		
+		List<Usuario> listUsuarios = usuarioService.findAll();
+		model.addAttribute("listUsuarios" ,listUsuarios);
+		LOGGER.info("Usuario registro {}", listUsuarios);
+		
+		return "administrador/usuarios";
 	}
 }
