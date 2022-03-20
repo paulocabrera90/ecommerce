d0bf456512d78e4272a33bdb6711debd5ba8c23d
Paulo Cabrera <paulocabrera90@gmail.com>
2022-03-20 01:52:22 -0300

    funcionalidad lista detalle orden admin

--- src/main/java/com/infosl/ecommerce/controller/AdministradorController.java
+++ src/main/java/com/infosl/ecommerce/controller/AdministradorController.java
@@ -1,17 +1,24 @@
 package com.infosl.ecommerce.controller;
 
 import java.util.List;
+import java.util.Optional;
+
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.infosl.ecommerce.model.Orden;
 import com.infosl.ecommerce.model.Producto;
 import com.infosl.ecommerce.model.Usuario;
+import com.infosl.ecommerce.service.detalleorden.IDetalleOrdenService;
+import com.infosl.ecommerce.service.orden.IOrdenService;
 import com.infosl.ecommerce.service.producto.IProductoService;
 import com.infosl.ecommerce.service.usuario.IUsuarioService;
  
@@ -24,6 +31,10 @@ public class AdministradorController {
 	private IProductoService productoService;
 	@Autowired
 	private IUsuarioService usuarioService;
+	@Autowired
+	private IOrdenService iOrdenService;
+	@Autowired
+	private IDetalleOrdenService iDetalleOrdenService;
 	
 	@GetMapping("")
 	public String home(Model model) {
@@ -42,5 +53,27 @@ public class AdministradorController {
 		LOGGER.info("Usuario registro {}", listUsuarios);
 		
 		return "administrador/usuarios";
+	}
+	
+	@GetMapping("/ordenes")
+	public String findOrdenes(Model model) {
+		
+//		List<Usuario> listUsuarios = usuarioService.findAll();
+		model.addAttribute("listOrdenes" ,iOrdenService.findAll());
+//		LOGGER.info("Usuario registro {}", listUsuarios);
+		
+		return "administrador/ordenes";
+	}
+	
+	@GetMapping("/detalle/{id}")
+	public String detalleOrden(@PathVariable Integer id, Model model) {
+		
+		Optional<Orden> orden = iOrdenService.findById(id);
+		
+		model.addAttribute("detallesList", orden.get().getListDetalle());		
+		
+		LOGGER.info("id de la orden {}", id);	
+		
+		return "administrador/detalleorden";
 	}
 }
